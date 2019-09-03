55f74d1d34a13840b97ea4d1a25e84f95437e1a5
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-03 17:51:03 +0700

    Describing UI structure

--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,20 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <div>
-    <button />
-    <code />
-    <input />
-    <label />
-    <p />
-    <pre />
-    <select />
-    <table />
-    <ul />
-  </div>,
+  <section>
+    <header>
+      <h1>A Header</h1>
+    </header>
+    <nav>
+      <a href="item">Nav Item</a>
+    </nav>
+    <main>
+      <p>The main content...</p>
+    </main>
+    <footer>
+      <small>&copy; 2018</small>
+    </footer>
+  </section>,
   document.getElementById('root'),
 );
 
