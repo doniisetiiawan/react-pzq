e2eec81596b94a992b59ec0cfaacc11d497063c0
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-08 10:07:26 +0700

    Dynamic property value

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import MyComponent from './MyComponent';
+const enabled = false;
+const text = 'A Button';
+const placeholder = 'input value...';
+const size = 50;
 
 render(
-  <MyComponent>
-    <MyComponent.First />
-    <MyComponent.Second />
-  </MyComponent>,
+  <section>
+    <button type="button" disabled={!enabled}>
+      {text}
+    </button>
+    <input placeholder={placeholder} size={size} />
+  </section>,
   document.getElementById('root'),
 );
