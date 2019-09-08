9722e61e40e239f74bd5acee4a01a98dac5b72cf
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-08 12:03:25 +0700

    Fragments of JSX

---
+++ src/WithFragments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WithFragments() {
+  return (
+    <>
+      <h1>With Fragments</h1>
+      <p>Doesn't have any unused DOM elements.</p>
+    </>
+  );
+}
+
+export default WithFragments;
--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const array = ['First', 'Second', 'Third'];
-
-const object = {
-  first: 1,
-  second: 2,
-  third: 3,
-};
+import WithoutFragments from './WithoutFragments';
+import WithFragments from './WithFragments';
 
 render(
-  <section>
-    <h1>Array</h1>
-
-    <ul>
-      {array.map((i) => (
-        <li key={i}>{i}</li>
-      ))}
-    </ul>
-    <h1>Object</h1>
-
-    <ul>
-      {Object.keys(object).map((i) => (
-        <li key={i}>
-          <strong>{i}: </strong>
-          {object[i]}
-        </li>
-      ))}
-    </ul>
-  </section>,
+  <div>
+    <WithoutFragments />
+    <WithFragments />
+  </div>,
   document.getElementById('root'),
 );
