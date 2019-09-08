182cc7b22a0d121de3618552f64c147c7e7c9233
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-08 12:42:14 +0700

    Default property

--- src/MyButton.jsx
+++ src/MyButton.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export default function MyButton(props) {
-  const { children } = props;
-  return <button type="button">{children}</button>;
+  const {disabled, text} = props;
+
+  return (
+    <button type="button" disabled={disabled}>
+      {text}
+    </button>
+  );
 }
+
+MyButton.defaultProps = {
+  disabled: false,
+  text: 'My Button',
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import MyComponent from './MyComponent';
+import MyButton from './MyButton';
 
-const myComponent = render(<MyComponent />, document.getElementById('root'));
-
-setTimeout(() => {
-  myComponent.setState({ first: 'done!' });
-}, 1000);
-
-setTimeout(() => {
-  myComponent.setState({ second: 'done!' });
-}, 2000);
-
-setTimeout(() => {
-  myComponent.setState({ third: 'done!' });
-}, 3000);
-
-setTimeout(() => {
-  myComponent.setState((state) => ({
-    ...state,
-    fourth: state.doneMessage,
-  }));
-}, 4000);
+render(<MyButton />, document.getElementById('root'));
