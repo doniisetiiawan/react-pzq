b8259d59f250df40eaf936d76b055c2b28607784
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-12 10:06:35 +0700

    Store provider and routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Root from './components/Root';
+import registerServiceWorker from './serviceWorker';
 
-import App from './App';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
