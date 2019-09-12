f491c561bee4d008b072fd741e8fcb13b3eff97e
Doni Setiawan <donisetiawan.050@gmail.com>
2019-09-12 12:23:26 +0700

    Refactor store

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import store from '../store';
+import store from '../store/store';
 import App from './App';
 
-export default () => (
+const Root = () => (
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+export default Root;
--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,10 @@ import { combineReducers } from 'redux-immutable';
 
 import initialState from './initialState';
 import App from './App';
-import Home from './Home';
-import Article from './Article';
 
 export default createStore(
   combineReducers({
     App,
-    Home,
-    Article,
   }),
   initialState,
 );
